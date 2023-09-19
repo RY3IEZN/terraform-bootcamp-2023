8bd13e37b14fce99937f9fdbd7ec4e14a620e165
unknown <unekue2@gmail.com>
2023-09-19 15:34:25 +0100

    testing out tf cmds and thr random resource

--- main.tf
+++ main.tf
@@ -1,1 +1,21 @@
 
+terraform {
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "3.5.1"
+    }
+  }
+}
+
+# create random string
+resource "random_string" "bucket_name" {
+  length           = 16
+  special          = true
+  override_special = "/@£$"
+}
+
+# create output to see name
+output "randomly_gen_name" {
+  value = random_string.bucket_name.id
+}
