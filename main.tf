59e573c9790f69f28ee544072f1b64cca19eaeeb
Uneku Ejiga <73601265+RY3IEZN@users.noreply.github.com>
2023-09-19 21:46:56 +0100

    #4 succefully created a bucket (#5)

--- main.tf
+++ main.tf
@@ -1,18 +1,14 @@
-
-terraform {
-  required_providers {
-    random = {
-      source  = "hashicorp/random"
-      version = "3.5.1"
-    }
-  }
-}
-
 # create random string
 resource "random_string" "bucket_name" {
-  length           = 16
-  special          = true
-  override_special = "/@£$"
+  length  = 16
+  upper   = false
+  special = false
+}
+
+
+# create the terauckets
+resource "aws_s3_bucket" "towntera_buckets" {
+  bucket = random_string.bucket_name.result
 }
 
 # create output to see name
---
+++ providers.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "3.5.1"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.17.0"
+    }
+  }
+}
+
+provider "aws" {
+  region     = "if you send me the location"
+  access_key = "denied"
+  secret_key = "revoked lol"
+}
