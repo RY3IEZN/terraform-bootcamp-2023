9791ccd08450431717200b6801150ed3959d1893
Uneku Ejiga <73601265+RY3IEZN@users.noreply.github.com>
2023-09-25 11:42:55 +0100

    8 create cloud front distribution (#9)
    * succesfully setup cdn
    
    * #8 added poc

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,9 @@
 output "s3_teratown_url" {
-  description = "s3 teratown url"
+  description = "s3 bucket teratown url"
   value       = module.terrahouse.s3_teratown_url
+}
+
+output "cloudfront_url" {
+  description = "cloud front url"
+  value       = module.terrahouse.cloudfront_url
 }
--- providers.tf
+++ providers.tf
@@ -15,6 +15,6 @@ provider "aws" {
   region     = ""
   access_key = ""
   secret_key = ""
-  delete_me  = ""
+  secret_key = ""
 }
 
